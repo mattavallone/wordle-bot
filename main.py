e49c4f17a883c294ba55c18293ff24b06292d1db
Matthew Avallone <mva271@nyu.edu>
2022-06-18 00:16:30 -0400

    implemented game play mechanics

--- main.py
+++ main.py
@@ -26,22 +26,23 @@ if __name__ == '__main__':
 	# print(allWordsScores)
 
 	# Get today's wordle
-	answer = solution.today()
-
+	answer, gameId = solution.today()
+	
 	# Have player solve wordle
-	state = []
-	attempt = 1
+	states = []
 	guesses = []
-	for attempt in range(5):
-		guess = player.computeGuess(solWordScores, attempt, guesses, state)
+	for attempt in range(1, 7):
+		guess = player.computeGuess(solWordScores, attempt, guesses, states)
+		print("Guess is " + guess + ", Score is " + str(solWordScores[guess]))
 
 		guesses.append(guess)
 
 		outcome = player.solve(answer, guess)
+		
+		states.append(outcome["verdict"])
 
 		if outcome["win"]:
-			print("We have a winner! Wordle is " + guess)
+			print("We have a winner! Wordle is " + guess + ", Number of Attempts: " + str(attempt))
 			break
-		else:
-			attempt += 1
-			state = outcome["verdict"]
+
+	player.postResults(states, gameId)
--- player.py
+++ player.py
@@ -8,6 +8,7 @@ Author: Matthew Avallone
 
 from enum import Enum 
 import numpy as np
+import requests
 
 # Enum for letter verdicts
 class LetterVerdict(Enum):
@@ -15,25 +16,57 @@ class LetterVerdict(Enum):
 	YELLOW = 2
 	GRAY = 3
 
-def computeGuess(wordDict, attempt, guesses, verdict):
+def computeGuess(wordDict, attempt, guesses, verdicts):
 	guess = ""
 	words = np.array(list(wordDict.keys()))
 
+	if attempt == 1:
+		guess = words[0] # grab the top scoring word as the first guess
+	else:
+		guess = ""
 
-	match attempt:
-		case 1:
-			guess = words[0] # grab the top scoring word as the first guess
-		case 2:
-			# possibleWords = np.where()
-			guess = ""
-		case 3:
-			guess = ""
-		case 4:
-			guess = ""
-		case 5:
-			guess = ""
-		case 6:
-			guess = ""
+		grays = []
+		greens = []
+		yellows = []
+		latestGuess = guesses[-1]
+		for j, verdict in enumerate(verdicts):
+			for i, v in enumerate(verdict):
+				if v == LetterVerdict.GRAY:
+					if guesses[j][i] not in grays:
+						grays.append(guesses[j][i])
+				if v == LetterVerdict.GREEN:
+					if (i, guesses[j][i]) not in greens:
+						greens.append((i, guesses[j][i]))
+				if v == LetterVerdict.YELLOW:
+					if (i, guesses[j][i]) not in yellows:
+						yellows.append((i, guesses[j][i]))
+
+		for word in words:
+			if word in guesses:
+				continue
+
+			validLetters = 0 # counter to denote when we've found our guess
+			for greenIndex, greenLetter in greens:
+				if word[greenIndex] != greenLetter:
+					break
+				else:
+					validLetters += 1
+
+			for yellowIndex, yellowLetter in yellows:
+				if yellowLetter not in word:
+					break
+				elif yellowIndex != word.find(yellowLetter):
+					validLetters += 1
+
+			for grayLetter in grays:
+				if(grayLetter in word):
+					validLetters -= 1
+
+			# check if word contains all known letters to be in word
+			expectedLetters = len(greens) + len(yellows)
+			if validLetters == expectedLetters:
+				guess = word
+				break
 
 	return guess
 
@@ -59,3 +92,25 @@ def solve(answer, guess):
 		result["verdict"] = verdict
 
 	return result
+
+def postResults(states, gameId):
+	'''
+	post results to a Discord server
+	'''
+	numAttempts = len(states)
+	message = "Wordle " + str(gameId) + " " + str(numAttempts) + "/6\n\n"
+	for row in states:
+		for i in row:
+			if i == LetterVerdict.GRAY:
+				message += ":black_large_square:"
+			if i == LetterVerdict.GREEN:
+				message += ":green_square:"
+			if i == LetterVerdict.YELLOW:
+				message += ":yellow_square:"
+		message += "\n"
+
+	payload = {
+		"content": message
+	}
+
+	requests.post("https://discord.com/api/webhooks/987568689001951252/BGny6ZSGMAR8o_ueA9wSS_NHJe_CMnkNVlSnr-iiqArUzYg7NjyEAVsTtZ3Nj3W075I1", data=payload)
